7e93a9ed658635d7bec63235d48698e561465af4
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-04-09 09:55:55 +0200

    create mobile screen

---
+++ lib/responsive/mobile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class mobileScreen extends StatefulWidget {
+  const mobileScreen({super.key});
+
+  @override
+  State<mobileScreen> createState() => _webscreenState();
+}
+
+class _mobileScreenState extends State<webscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: ,
+    )
+  }
+}

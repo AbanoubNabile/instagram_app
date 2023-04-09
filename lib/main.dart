82e40272322c8dd1ffd0f2e3242f99759f43ec7e
Abanoub Nabil <126210906+AbanoubNabile@users.noreply.github.com>
2023-04-09 08:47:09 +0200

    add text web view and mobil view

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_app/Home.dart';
+import 'dart:ui' show lerpDouble;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
